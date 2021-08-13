88535e0d2ba81d82b0b4424bd8771652237cb64b
VarenytsiaMykhailo <52725148+VarenytsiaMykhailo@users.noreply.github.com>
2021-08-14 02:44:16 +0300

    WikiCrawler

--- myCode/src/com/github/varenytsiamykhailo/thinkdast/JedisIndex.java
+++ myCode/src/com/github/varenytsiamykhailo/thinkdast/JedisIndex.java
@@ -98,6 +98,37 @@ public class JedisIndex {
     }
 
     /**
+     * Looks up a term and returns a map from URL to count.
+     *
+     * @param term
+     * @return Map from URL to count.
+     */
+    public Map<String, Integer> getCountsFaster(String term) {
+        // convert the set of strings to a list so we get the
+        // same traversal order every time
+        List<String> urls = new ArrayList<String>();
+        urls.addAll(getURLs(term));
+
+        // construct a transaction to perform all lookups
+        Transaction t = jedis.multi();
+        for (String url: urls) {
+            String redisKey = termCounterKey(url);
+            t.hget(redisKey, term);
+        }
+        List<Object> res = t.exec();
+
+        // iterate the results and make the map
+        Map<String, Integer> map = new HashMap<String, Integer>();
+        int i = 0;
+        for (String url: urls) {
+            System.out.println(url);
+            Integer count = new Integer((String) res.get(i++));
+            map.put(url, count);
+        }
+        return map;
+    }
+
+    /**
      * Returns the number of times the given term appears at the given URL.
      *
      * @param url
